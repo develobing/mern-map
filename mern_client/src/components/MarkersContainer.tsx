0f7659ff7103619bb59a2cf03a303a37b57ec84c
Robin <philoin88@gmail.com>
2024-03-17 02:49:38 +0900

    맵에 마커 추가

---
+++ mern_client/src/components/MarkersContainer.tsx
@@ -0,0 +1,44 @@
+import { useAtom, useAtomValue } from 'jotai';
+import React from 'react';
+import { mapAtom } from '../atoms/map';
+import { infosAtom, selectInfoAtom } from '../atoms/info';
+import { Info } from '../types/info';
+import Marker from './common/Marker';
+
+const MarkersContainer = () => {
+  const map = useAtomValue(mapAtom);
+  const infos = useAtomValue(infosAtom);
+  const [selectInfo, setSelectInfo] = useAtom(selectInfoAtom);
+
+  if (!map || !infos) return null;
+
+  return (
+    <>
+      {infos?.map((info: Info) => (
+        <Marker
+          key={info.id}
+          map={map}
+          position={info.position}
+          content={`<div class="marker" />`}
+          onClick={() => {
+            setSelectInfo(info);
+          }}
+        />
+      ))}
+
+      {selectInfo && (
+        <Marker
+          key={selectInfo.id}
+          map={map}
+          position={selectInfo.position}
+          content={`<div class="marker select" />`}
+          onClick={() => {
+            setSelectInfo(null);
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default MarkersContainer;
--- mern_client/src/pages/Home/index.tsx
+++ mern_client/src/pages/Home/index.tsx
@@ -1,11 +1,22 @@
+import { useSetAtom } from 'jotai';
+import { infosAtom } from '../../atoms/info';
 import MapContainer from '../../components/MapContainer';
 import Navigation from '../../components/Navigation';
+import { infos } from '../../data/infos';
+import MarkersContainer from '../../components/MarkersContainer';
 
 function Home() {
+  const setInfos = useSetAtom(infosAtom);
+
+  if (infos) {
+    setInfos(infos);
+  }
+
   return (
     <>
       <Navigation />
       <MapContainer />
+      <MarkersContainer />
     </>
   );
 }
